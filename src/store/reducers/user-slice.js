a20b1a66525ed346e27fa3284cd5eeff12e515a9
sdnsp0124 <sdnsp0124@gmail.com>
2025-08-28 11:28:20 +0900

    store 구성

---
+++ src/store/reducers/user-slice.js
@@ -0,0 +1,16 @@
+import { createSlice } form "@reduxjs/toolkit"
+
+const iniState = {
+  isLogging: false,
+  isLogOn: false,
+  data: {} 
+}
+
+const userSlice = createSlice({
+  name: "user"
+  intialState,
+  reducer:
+  // sync
+
+  // async
+})
