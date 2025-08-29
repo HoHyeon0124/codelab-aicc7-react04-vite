421e409591991eb07aace7eb222fcdbd61335a0b
sdnsp0124 <sdnsp0124@gmail.com>
2025-08-29 14:16:11 +0900

    useSWR Provider 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "font-awesome/css/font-awesome.min.css"
+import { SWRConfig } from "swr"
 import { BrowserRouter } from "react-router-dom"
 import { Provider as StoreProvider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react"
@@ -8,16 +9,19 @@ import "../public/css/base.css"
 import "../public/css/shop.css"
 import TestProvider from "./providers/TestProviders"
 import Containers from "./Containers"
+import { swrValue } from "./swr"
 
 function App() {
   return (
     <StoreProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <TestProvider>
-            <Containers />
-          </TestProvider>
-        </BrowserRouter>
+        <SWRConfig value={swrValue}>
+          <BrowserRouter>
+            <TestProvider>
+              <Containers />
+            </TestProvider>
+          </BrowserRouter>
+        </SWRConfig>
       </PersistGate>
     </StoreProvider>
   )
