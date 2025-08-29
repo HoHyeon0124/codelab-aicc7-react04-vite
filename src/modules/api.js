ce67d67418c53204706692d46575a48448e268b2
sdnsp0124 <sdnsp0124@gmail.com>
2025-08-29 11:43:09 +0900

    BoardList.js

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,12 +1,10 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
   timeout: 5000,
 })
 
 const dataApi = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
   timeout: 5000,
 })
 
@@ -14,7 +12,7 @@ api.interceptors.request.use(
   (config) => {
     const authString = sessionStorage.getItem("persist:auth")
     const auth = JSON.parse(authString)
-    const { token } = auth?.data?.token || "ABCDE"
+    const token = auth?.data?.token || "ABCDE"
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
