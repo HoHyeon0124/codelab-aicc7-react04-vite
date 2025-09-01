c6f94265e2c7e976dd0ce9fbb1612a5bf29cf75d
sdnsp0124 <sdnsp0124@gmail.com>
2025-09-01 16:28:41 +0900

    chat 완성

--- src/components/chat/ChatWrapper.jsx
+++ src/components/chat/ChatWrapper.jsx
@@ -1,7 +1,9 @@
 import styled from "@emotion/styled"
 import { css } from "@emotion/react"
 import { Button, TextField } from "@mui/material"
-import { useState, useRef } from "react"
+import { useState, useRef, useContext, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { FirebaseContext } from "@/providers/FirebaseProvider"
 
 const WrapperRoot = styled.div``
 const ChatInputWrap = styled.div`
@@ -39,27 +41,24 @@ const active = css`
   align-self: flex-end;
   background-color: #6ac030;
 `
-const dummy = [
-  { id: 1, name: "홍길동", comment: "가나", isMine: false },
-  { id: 2, name: "홍길만", comment: "다라", isMine: false },
-  { id: 3, name: "홍길동", comment: "마바", isMine: false },
-  { id: 4, name: "호현", comment: "가나다라마바사아", isMine: true },
-  { id: 5, name: "홍길순", comment: "사아", isMine: false },
-]
-
 export default function ChatWrapper({ children }) {
-  const [list, setList] = useState([...dummy])
+  const { isLogOn, user } = useSelector((state) => state.auth)
+  const { rtRef, onRtValue, rtPush, rtdb } = useContext(FirebaseContext)
   const [comment, setComment] = useState("")
-  const [focused, setFocused] = useState(true)
+  const [list, setList] = useState([])
   const inputRef = useRef(null)
+  const commentRef = rtRef(rtdb, "root/comment")
+
   // TODO :: 데이터를 저장, Input을 비워준다. Input에 focus
   const onClickButton = (e) => {
-    setList((prev) => [
-      ...prev,
-      { id: prev[prev.length - 1].id + 1, name: "호현", comment, isMine: true },
-    ])
-    setComment("")
-    setFocused(true)
+    if (comment.length > 0) {
+      rtPush(commentRef, {
+        uid: user?.uid || "",
+        name: user?.name || "",
+        comment,
+      })
+      setComment("")
+    }
   }
   // TODO :: shift(o) -> 아무일 안일어남. shift(x) -> 전송
   const onKeyDownInput = (e) => {
@@ -68,6 +67,18 @@ export default function ChatWrapper({ children }) {
       onClickButton()
     }
   }
+
+  useEffect(() => {
+    const unsubscribe = onRtValue(commentRef, (snapshot) => {
+      const dataArr = Object.values(snapshot?.val()).map((item) => {
+        item.isMine = item.uid === user.uid
+        return item
+      })
+      setList(dataArr)
+    })
+    return unsubscribe
+  }, [])
+
   return (
     <WrapperRoot>
       <ChatInputWrap>
@@ -83,14 +94,13 @@ export default function ChatWrapper({ children }) {
           onChange={(e) => setComment(e.target.value)}
           value={comment}
           autoFocus
-          focused={focused}
         />
         <Button variant="contained" onClick={onClickButton}>
           확인
         </Button>
       </ChatInputWrap>
       <ChatListWrap>
-        {list.map((item, idx) => (
+        {[...list].reverse().map((item, idx) => (
           <ChatList key={idx} css={item.isMine ? active : null}>
             <ChatName>{item.name}</ChatName>
             <ChatComment>{item.comment}</ChatComment>
