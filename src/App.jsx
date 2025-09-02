6bb7f008313e287807c637fea9bc9f60708edf9d
sdnsp0124 <sdnsp0124@gmail.com>
2025-09-02 17:50:26 +0900

    Alet -> Provider로 이전

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,29 @@ import { BrowserRouter } from "react-router-dom"
 import { Provider as StoreProvider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react"
 import { store, persistor } from "./store"
-// import "../public/css/fonts.css"
-// import "../public/css/base.css"
-// import "../public/css/shop.css"
+import FirebaseProvider from "./providers/FirebaseProvider"
 import TestProvider from "./providers/TestProviders"
 import Containers from "./Containers"
 import { swrValue } from "./swr"
-import FirebaseProvider from "./providers/FirebaseProvider"
 
 import "@/assets/styles/app.scss"
+import AlertProvider from "./providers/AlertProvider"
 
 function App() {
   return (
     <StoreProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <SWRConfig value={swrValue}>
-          <FirebaseProvider>
-            <BrowserRouter>
-              <TestProvider>
-                <Containers />
-              </TestProvider>
-            </BrowserRouter>
-          </FirebaseProvider>
-        </SWRConfig>
+        <AlertProvider>
+          <SWRConfig value={swrValue}>
+            <FirebaseProvider>
+              <BrowserRouter>
+                <TestProvider>
+                  <Containers />
+                </TestProvider>
+              </BrowserRouter>
+            </FirebaseProvider>
+          </SWRConfig>
+        </AlertProvider>
       </PersistGate>
     </StoreProvider>
   )
